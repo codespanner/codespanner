ad3451f0b0e6247fc3ab2deedb955090b5822af4
mdshakeer <mdshakeer91@gmail.com>
2014-06-29 19:28:37 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maeru::Application.config.secret_token = '464c0771e6bc22da0e3f4b4236517ab6dbad1427f58d0d2085e3098896f15a0521ffeb500e18b9b00b4b631f4c3bbd8943abf4715c07bec4fcaaf87ea40f2bbd'
